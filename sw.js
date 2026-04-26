27b508f6e33246c8a272a7970446514b8c20c932
jatlamine33-ui <jatlamine33@gmail.com>
2026-04-26 01:19:47 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,19 @@
-const CACHE = 'fitland-v1';
-const ASSETS = [
-  '/fitland_moknine_app.html',
-];
+const CACHE = 'fitland-v2';
+const ASSETS = ['/'];
 
 self.addEventListener('install', e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));
+});
+
+self.addEventListener('activate', e=>{
   e.waitUntil(
-    caches.open(CACHE).then(c=>c.addAll(ASSETS))
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+    ))
   );
 });
 
 self.addEventListener('fetch', e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
